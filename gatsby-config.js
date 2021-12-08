4c0643f98c9a38e9ef739b64f51637e197fb3f9b
iwashita <iwashita@supersoftware.co.jp>
2021-12-08 17:52:07 +0900

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "get-user-media-test",
+    siteUrl: "https://iwashi1t.github.io/gum-test/",
+    title: "gum-test",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  pathPrefix: "/gum-test",
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-sass'
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,183 +1,156 @@
-import * as React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import './styles.scss'
+import testImage from '../images/test.png'
+import frameImage from '../images/img10-f.png'
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const FRAME_HEIGHT = 640
+const FRAME_WIDTH = 480
+const frameAspect = FRAME_HEIGHT / FRAME_WIDTH
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
+  const video = useRef()
+  const canvas = useRef()
+
+  const [downloadURL, setDownloadURL] = useState()
+
+  const takePhoto = () => {
+    video.current.pause()
+
+    const context = canvas.current.getContext('2d')
+
+    const videoStyle = window.getComputedStyle(video.current)
+    const videoViewWidth = parseFloat(videoStyle.width)
+    const videoViewHeight = parseFloat(videoStyle.height)
+    console.log(`*** videoView width=${videoViewWidth} height=${videoViewHeight}`)
+    // const canvasStyle = window.getComputedStyle(canvas.current)
+    // console.log(`*** canvas width=${canvasStyle.width} height=${canvasStyle.height}`)
+    // const frameStyle = window.getComputedStyle(frame.current)
+    // console.log(`*** frame width=${frameStyle.width} height=${frameStyle.height}`)
+
+    const { videoWidth, videoHeight } = video.current
+    console.log(`*** video width=${videoWidth} height=${videoHeight}`)
+
+    const videoAspect = (videoHeight / videoWidth)
+    const videoViewAspect = (videoViewHeight / videoViewWidth)
+
+    const [frameViewWidth, frameViewHeight] = (videoViewAspect > frameAspect)
+      ? [videoViewWidth, videoViewWidth * frameAspect]
+      : [videoViewHeight / frameAspect, videoViewHeight]
+    console.log(`*** frameView width=${frameViewWidth} height=${frameViewHeight}`)
+
+    const videoRatio = (videoAspect > videoViewAspect)
+      ? (videoWidth / videoViewWidth)
+      : (videoHeight / videoViewHeight)
+    console.log(`*** videoRatio=${videoRatio}`)
+
+    const w = frameViewWidth * videoRatio
+    const h = frameViewHeight * videoRatio
+    const x = (videoWidth - w) / 2
+    const y = (videoHeight - h) / 2
+
+    console.log(`*** drawImage x=${x} y=${y} w=${w} h=${h}`)
+    context.drawImage(video.current, x, y, w, h, 0, 0, FRAME_WIDTH, FRAME_HEIGHT)
+
+    canvas.current.toBlob(blob => {
+      console.log(`*** blob`, blob)
+      const url = URL.createObjectURL(blob)
+      setDownloadURL(url)
+    })
+  }
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+      .then(stream => {
+        video.current.srcObject = stream
+
+        setTimeout(takePhoto, 3000)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
+    <div>
+      <video
+        ref={ref => video.current = ref}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'block',
+          objectFit: 'cover'
+        }}
+        autoPlay
+        playsInline
+        muted
+        poster={testImage}
+        // width={IMAGE_WIDTH}
+        // height={IMAGE_HEIGHT}
+        // onLoadedMetadata={() => {
+        //   console.log(`*** height=${video.current.videoHeight} width=${video.current.videoWidth}`)
+        // }}
       />
-    </main>
+      <canvas
+        ref={ref => canvas.current = ref}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'block',
+          objectFit: 'contain'
+        }}
+        width={FRAME_WIDTH}
+        height={FRAME_HEIGHT}
+      />
+      <img
+        src={frameImage}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'block',
+          objectFit: 'contain'
+        }}
+        alt="Frame image"
+      />
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
+        <div style={{
+          height: 100
+        }}></div>
+        <div style={{
+          height: 100,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          {downloadURL &&
+            <a href={downloadURL} download >
+              DOWNLOAD
+            </a>
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
